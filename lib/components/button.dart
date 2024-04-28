fe3e3e6d0b33d88efc888c1dff6f71f4b7f212d7
iabzeet <iabzeet@gmail.com>
2024-04-29 00:44:55 +0530

    Intro Page UI

---
+++ lib/components/button.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final String text;
+  const MyButton({
+    super.key,
+    required this.text,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Color.fromARGB(212, 135, 81, 77),
+        borderRadius: BorderRadius.circular(40),
+      ),
+      padding: EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          //text
+          Text(
+            text,
+            style: TextStyle(color: Colors.white),
+          ),
+
+          const SizedBox(width: 10),
+
+          //icon
+          Icon(
+            Icons.arrow_forward,
+            color: Colors.white,
+          )
+        ],
+      ),
+    );
+  }
+}
